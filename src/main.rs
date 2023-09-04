fcfc08241c6c05dafb7a17f7e3d0beeb4152ad63
Ikey Doherty <ikey@serpentos.com>
2023-09-04 23:42:53 +0100

    main: Add `version` processing
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,17 @@ use clap::Command;
 
 /// TODO: Add actual subcommands!
 fn cli_main() -> Command {
-    Command::new("moss").about("Next generation package manager")
+    Command::new("moss")
+        .about("Next generation package manager")
+        .arg_required_else_help(true)
+        .subcommand_required(true)
+        .subcommand(Command::new("version").about("Display version and exit"))
 }
 
 /// Main entry point
 fn main() {
     match cli_main().get_matches().subcommand() {
+        Some(("version", _)) => println!("Gimme a break I only just started!"),
         _ => println!("We should implement a real CLI, huh?"),
     }
 }
