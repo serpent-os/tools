5fe767bf3739c9229362071b1d06839befb6ee04
Ikey Doherty <ikey@serpentos.com>
2023-09-04 23:38:04 +0100

    main: Use `clap` to define our main entry point
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
+use clap::Command;
+
+/// TODO: Add actual subcommands!
+fn cli_main() -> Command {
+    Command::new("moss").about("Next generation package manager")
+}
+
+/// Main entry point
 fn main() {
-    println!("TODO: Everything. Again ;)");
+    match cli_main().get_matches().subcommand() {
+        _ => println!("We should implement a real CLI, huh?"),
+    }
 }
