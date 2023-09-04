7a30b0256f5992bbd73245327627a1f230562706
Ikey Doherty <ikey@serpentos.com>
2023-09-05 00:19:32 +0100

    main: Add `-v`, `--version`
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,36 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-use clap::Command;
+use clap::{Arg, ArgAction, Command};
 
 /// TODO: Add actual subcommands!
 fn cli_main() -> Command {
     Command::new("moss")
         .about("Next generation package manager")
+        .arg(
+            Arg::new("version")
+                .short('v')
+                .long("version")
+                .action(ArgAction::SetTrue),
+        )
         .arg_required_else_help(true)
-        .subcommand_required(true)
         .subcommand(Command::new("version").about("Display version and exit"))
+}
+
+/// Print program version
+fn print_version() {
+    println!("TODO: Set a version");
 }
 
 /// Main entry point
 fn main() {
+    let matches = cli_main().get_matches();
+    if matches.get_flag("version") {
+        print_version();
+        return;
+    }
     match cli_main().get_matches().subcommand() {
-        Some(("version", _)) => println!("Gimme a break I only just started!"),
+        Some(("version", _)) => print_version(),
         _ => unreachable!(),
     }
 }
