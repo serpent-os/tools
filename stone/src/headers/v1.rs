fa9262bc6545833fb0c9c7c4859f15e8fdb79852
Ikey Doherty <ikey@serpentos.com>
2023-09-05 11:18:31 +0100

    stone/headers/v1: Docs
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- stone/src/headers/v1.rs
+++ stone/src/headers/v1.rs
@@ -2,12 +2,16 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
+/// Simple corruption check in the header, will be expanded for v2
 const INTEGRITY_CHECK: [u8; 21] = [
     0, 0, 1, 0, 0, 2, 0, 0, 3, 0, 0, 4, 0, 0, 5, 0, 0, 6, 0, 0, 7,
 ];
 
 ///
 /// Well known file type for a v1 stone container
+///
+/// Some types are now legacy as we're going to use Ion to define them.
+///
 #[repr(u8)]
 pub enum FileType {
     /// Sanity: Unknown container type
