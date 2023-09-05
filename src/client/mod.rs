bd8169500f1cae842d68b21ebb52c3cae4e423ec
Ikey Doherty <ikey@serpentos.com>
2023-09-05 01:07:58 +0100

    client: Add skeletal Client struct
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,3 +1,20 @@
 // SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
 //
 // SPDX-License-Identifier: MPL-2.0
+
+/// A Client is a connection to the underlying package management systems
+pub struct Client {}
+
+impl Client {
+    /// Construct a new Client
+    fn new() -> Client {
+        Client {}
+    }
+}
+
+impl Drop for Client {
+    // Automatically drop resources for the client
+    fn drop(&mut self) {
+        todo!()
+    }
+}
