e42cc4c78bd8292c33dd5c5833043210336ff36a
Ikey Doherty <ikey@serpentos.com>
2023-09-05 22:51:50 +0100

    stone: Add preliminary Reader
    This is largely noop but adds a bunch of scaffolding to allow us to read from
    a BufRead trait, dynamically loading the 32-byte header from an input stream
    or returning an IO error.
    
    We'll need to add our own results now as we need to define an error enum
    for the instance in which we find invalid magic strings.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ stone/src/reader.rs
@@ -0,0 +1,54 @@
+// SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+//
+// SPDX-License-Identifier: MPL-2.0
+
+use core::slice;
+use std::{
+    io::{BufRead, Result},
+    mem::{size_of, zeroed},
+};
+
+use crate::headers::versioning::{AgnosticHeader, Version};
+
+#[cfg(test)]
+mod reader_tests {
+    use super::new;
+
+    /// Header for bash completion stone archive
+    const BASH_TEST_STONE: [u8; 32] = [
+        0x0, 0x6d, 0x6f, 0x73, 0x0, 0x4, 0x0, 0x0, 0x1, 0x0, 0x0, 0x2, 0x0, 0x0, 0x3, 0x0, 0x0,
+        0x4, 0x0, 0x0, 0x5, 0x0, 0x0, 0x6, 0x0, 0x0, 0x7, 0x1, 0x0, 0x0, 0x0, 0x1,
+    ];
+
+    /// Legacy manifest archive
+    const TEST_MANIFEST: [u8; 32] = [
+        0x0, 0x6d, 0x6f, 0x73, 0x0, 0x1, 0x0, 0x0, 0x1, 0x0, 0x0, 0x2, 0x0, 0x0, 0x3, 0x0, 0x0,
+        0x4, 0x0, 0x0, 0x5, 0x0, 0x0, 0x6, 0x0, 0x0, 0x7, 0x4, 0x0, 0x0, 0x0, 0x1,
+    ];
+
+    #[test]
+    fn test_versioning() {
+        // Construct a reader from a byte sequence
+        let mut reader: Box<dyn std::io::BufRead> = Box::new(&BASH_TEST_STONE[..]);
+        let _ = new(&mut reader).unwrap();
+    }
+}
+
+///
+/// Create a new reader for the given byte sequence
+///
+pub fn new(bytes: &mut dyn BufRead) -> Result<()> {
+    let mut hdr: AgnosticHeader = unsafe { zeroed::<AgnosticHeader>() };
+
+    // Grab the AgnosticHeader from the 32-byte header
+    let slice = unsafe {
+        slice::from_raw_parts_mut(&mut hdr as *mut _ as *mut u8, size_of::<AgnosticHeader>())
+    };
+    bytes.read_exact(slice)?;
+
+    // Testing: Ensure we're reading V1.
+    #[cfg(test)]
+    assert_eq!(hdr.version(), Version::V1);
+
+    Ok(())
+}
