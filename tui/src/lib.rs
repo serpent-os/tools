e409a521be2c0203650a840a6685f974cc4e82c5
Ikey Doherty <ikey@serpentos.com>
2023-09-22 14:13:20 +0100

    tui: Add new term_size() helper to get sane fallback constraints
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- tui/src/lib.rs
+++ tui/src/lib.rs
@@ -10,6 +10,28 @@ mod program;
 mod runtime;
 pub mod widget;
 
+const DEFAULT_TERM_SIZE: (u16, u16) = (80, 24);
+
+/// Simple terminal constraints wrapping
+pub struct TermSize {
+    pub width: usize,
+    pub height: usize,
+}
+
+/// Generate a sane-fallback TermSize
+pub fn term_size() -> TermSize {
+    let size = crossterm::terminal::size().unwrap_or(DEFAULT_TERM_SIZE);
+    let mapped = if size.0 < 1 || size.1 < 1 {
+        DEFAULT_TERM_SIZE
+    } else {
+        size
+    };
+    TermSize {
+        width: mapped.0 as usize,
+        height: mapped.1 as usize,
+    }
+}
+
 /// Provide a standard approach to ratatui based TUI in moss
 mod reexport {
     use std::io::Stdout;
