7dd3dc413885dd050f6c54a953d58312c1a16080
Gavin Zhao <git@gzgz.dev>
2023-09-24 21:15:59 -0400

    Remove draft code
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

--- dag/src/lib.rs
+++ dag/src/lib.rs
@@ -1,2 +1,1 @@
-pub mod topo;
 pub mod subgraph;
--- dag/src/subgraph.rs
+++ dag/src/subgraph.rs
@@ -12,7 +12,11 @@ where
     for starting_node in starting_nodes {
         dfs.move_to(starting_node);
         while let Some(node) = dfs.next(&graph) {
-            res.extend(graph.neighbors(node).map(|adj| (node, adj)));
+            res.extend(
+                graph
+                    .neighbors_directed(node, petgraph::Direction::Outgoing)
+                    .map(|adj| (node, adj)),
+            );
         }
     }
 
@@ -23,7 +27,7 @@ where
 mod test {
     use petgraph::{
         prelude::DiGraphMap,
-        visit::{Topo, Walker},
+        visit::{Reversed, Topo, Walker},
     };
 
     use super::*;
@@ -31,10 +35,33 @@ mod test {
     #[test]
     fn basic_topo() {
         let graph: DiGraphMap<i32, ()> = DiGraphMap::from_edges(&[(1, 2), (1, 3), (2, 3)]);
-        let subgraph = subgraph(&graph, vec![1]);
-        let topo = Topo::new(&subgraph);
-        let order: Vec<i32> = topo.iter(&subgraph).collect();
+        let subg = subgraph(&graph, vec![1]);
+        let topo = Topo::new(&subg);
+        let order: Vec<i32> = topo.iter(&subg).collect();
 
         assert_eq!(order, vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn reverse_topo() {
+        let graph: DiGraphMap<i32, ()> = DiGraphMap::from_edges(&[(1, 2), (1, 3), (2, 3)]);
+        let items = vec![1];
+        let subg = subgraph(&graph, items);
+        let revg = Reversed(&subg);
+        let removal: Vec<i32> = Topo::new(revg).iter(revg).collect();
+        assert_eq!(removal, vec![3, 2, 1]);
+    }
+
+    // TODO: break cycles!
+    #[ignore = "cycles breaking needs to be implemented"]
+    #[test]
+    fn cyclic_topo() {
+        let graph: DiGraphMap<i32, ()> =
+            DiGraphMap::from_edges(&[(1, 2), (1, 3), (2, 4), (2, 5), (3, 5), (4, 1)]);
+        let items = vec![1, 4];
+        let subg = subgraph(&graph, items);
+        let revg = Reversed(&subg);
+        let removal: Vec<i32> = Topo::new(revg).iter(revg).collect();
+        assert_eq!(removal, vec![5, 3, 4, 2, 1]);
     }
 }
