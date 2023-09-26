15336eb52c0b0b826a7dd60e32897ca04f271ffb
Ikey Doherty <ikey@serpentos.com>
2023-09-26 01:12:02 +0100

    moss/registry/transaction: Add skeleton (pre-graph) code
    We'll need these components in place in order to allow the correct
    search logic for local sets vs installed vs repo-wide, and its a
    fair chunk of change prior to bringing in the graph traversal
    code.
    
    In order to simplify things, we'll split this across multiple changes
    for documentary purposes.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- moss/src/registry/transaction.rs
+++ moss/src/registry/transaction.rs
@@ -2,14 +2,27 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-use futures::executor::block_on;
+use std::collections::HashSet;
+
+use futures::{executor::block_on, StreamExt, TryFutureExt};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-use crate::{db::meta::Database, package, Registry};
+use crate::{db::meta::Database, package, Package, Provider, Registry};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Id(u64);
+
+enum ProviderFilter {
+    /// Must be installed
+    InstalledOnly(Provider),
+
+    /// Filter the lookup to current selection scope
+    Selections(Provider),
+
+    // Look beyond installed/selections
+    All(Provider),
+}
 
 /// A Transaction is used to modify one system state to another
 #[derive(Clone, Debug)]
@@ -22,6 +35,9 @@ pub struct Transaction<'a> {
 
     /// Memory bound database for resolution
     db: Database,
+
+    // unique set of package ids
+    packages: HashSet<package::Id>,
 }
 
 /// Construct a new Transaction wrapped around the underlying Registry
@@ -32,23 +48,87 @@ pub(super) fn new(registry: &Registry) -> Result<Transaction<'_>, Error> {
         id: None,
         registry,
         db: block_on(Database::new("sqlite::memory:", false))?,
+        packages: HashSet::new(),
     })
 }
 
 impl<'a> Transaction<'a> {
-    /// Add packages, resolving dependencies
-    pub fn add(id: Vec<package::Id>) -> Result<(), Error> {
-        Err(Error::NotImplemented)
+    /// Add a package to this transaction
+    pub async fn add(&mut self, id: package::Id) -> Result<(), Error> {
+        self.packages.extend(self.compute_deps(id).await?);
+        Ok(())
     }
 
     /// Remove a set of packages and reverse dependencies
-    pub fn remove(id: Vec<package::Id>) -> Result<(), Error> {
+    pub fn remove(&self, id: package::Id) -> Result<(), Error> {
         Err(Error::NotImplemented)
     }
 
     /// Return the package IDs in the fully baked configuration
-    pub fn finalize() -> Result<Vec<package::Id>, Error> {
+    pub fn finalize(&self) -> Result<Vec<package::Id>, Error> {
         Err(Error::NotImplemented)
+    }
+
+    /// Return all of the dependencies for input ID
+    async fn compute_deps(&self, id: package::Id) -> Result<Vec<package::Id>, Error> {
+        let lookup = self.registry.by_id(&id).collect::<Vec<_>>().await;
+        let candidate = lookup.first().ok_or(Error::NoCandidate(id.into()))?;
+
+        Ok(vec![candidate.id.clone()])
+    }
+
+    /// Attempt to resolve the filterered provider
+    async fn resolve_provider(&self, filter: ProviderFilter) -> Result<package::Id, Error> {
+        match filter {
+            ProviderFilter::All(provider) => self
+                .registry
+                .by_provider(&provider, package::Flags::AVAILABLE)
+                .collect::<Vec<_>>()
+                .await
+                .first()
+                .map(|p| p.id.clone())
+                .ok_or(Error::NoCandidate(provider.to_string())),
+            ProviderFilter::InstalledOnly(provider) => self
+                .registry
+                .by_provider(&provider, package::Flags::INSTALLED)
+                .collect::<Vec<_>>()
+                .await
+                .first()
+                .map(|p| p.id.clone())
+                .ok_or(Error::NoCandidate(provider.to_string())),
+            ProviderFilter::Selections(provider) => self
+                .registry
+                .by_provider(&provider, package::Flags::NONE)
+                .filter_map(|f| async {
+                    if self.packages.contains(&f.id) {
+                        Some(f)
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<Package>>()
+                .await
+                .first()
+                .map(|p| p.id.clone())
+                .ok_or(Error::NoCandidate(provider.to_string())),
+        }
+    }
+
+    // Try all strategies to resolve a provider for installation
+    async fn resolve_installation_provider(
+        &self,
+        provider: Provider,
+    ) -> Result<package::Id, Error> {
+        self.resolve_provider(ProviderFilter::Selections(provider.clone()))
+            .or_else(|_| async {
+                self.resolve_provider(ProviderFilter::InstalledOnly(provider.clone()))
+                    .await
+            })
+            .or_else(|_| async {
+                self.resolve_provider(ProviderFilter::All(provider.clone()))
+                    .await
+            })
+            .await
     }
 }
 
@@ -56,6 +136,9 @@ impl<'a> Transaction<'a> {
 pub enum Error {
     #[error("database error: {0}")]
     Database(#[from] crate::db::meta::Error),
+
+    #[error("no such name: {0}")]
+    NoCandidate(String),
 
     #[error("not yet implemented")]
     NotImplemented,
