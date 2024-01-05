7cdc88fce968a2a1cf49a900f9ff796f85674645
Ikey Doherty <ikey@serpentos.com>
2024-01-05 21:21:57 +0000

    triggers: Introduce serde yaml format for Triggers
    This will form the foundation of our trigger system by providing
    a way to deserialise trigger definitions and match them via fnmatch
    strings.
    
    Note at this point we don't include the actual execution types within
    the trigger as its a ... point of fluidity.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ crates/triggers/src/format.rs
@@ -0,0 +1,87 @@
+// SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+//
+// SPDX-License-Identifier: MPL-2.0
+
+use std::collections::BTreeMap;
+
+use fnmatch::Pattern;
+use serde::Deserialize;
+
+/// Filter matched paths to a specific kind
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum PathKind {
+    Directory,
+    Symlink,
+}
+
+/// Execution handlers for a trigger
+#[derive(Debug, Deserialize, Clone)]
+#[serde(untagged)]
+pub enum Handler {
+    Run { run: String, args: Vec<String> },
+    Delete { delete: Vec<String> },
+}
+
+/// Inhibitors prevent handlers from running based on some constraints
+#[derive(Debug, Deserialize)]
+pub struct Inhibitors {
+    pub paths: Vec<String>,
+    pub environment: Vec<String>,
+}
+
+/// Map handlers to a path pattern and kind filter
+#[derive(Debug, Deserialize)]
+pub struct PathDefinition {
+    pub handlers: Vec<String>,
+    #[serde(rename = "type")]
+    pub kind: PathKind,
+}
+
+/// Serialiazation format of triggers
+#[derive(Debug, Deserialize)]
+pub struct Trigger {
+    /// Unique (global scope) identifier
+    pub name: String,
+
+    /// User friendly description
+    pub description: String,
+
+    /// Run before this trigger name
+    pub before: Option<String>,
+
+    /// Run after this trigger name
+    pub after: Option<String>,
+
+    /// Optional inhibitors
+    pub inhibitors: Option<Inhibitors>,
+
+    /// Map glob / patterns to their configuration
+    pub paths: BTreeMap<Pattern, PathDefinition>,
+
+    /// Named handlers within this trigger scope
+    pub handlers: BTreeMap<String, Handler>,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::format::Trigger;
+
+    #[test]
+    fn test_trigger_file() {
+        let trigger: Trigger =
+            serde_yaml::from_str(include_str!("../../../test/trigger.yml")).unwrap();
+
+        let (pattern, _) = trigger.paths.iter().next().expect("Missing path entry");
+        let result = pattern
+            .match_path("/usr/lib/modules/6.6.7-267.current/kernel")
+            .expect("Couldn't match path");
+        let version = result
+            .variables
+            .get("version")
+            .expect("Missing kernel version");
+        assert_eq!(version, "6.6.7-267.current", "Wrong kernel version match");
+        eprintln!("trigger: {trigger:?}");
+        eprintln!("match: {result:?}");
+    }
+}
