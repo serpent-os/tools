e0a56bece9b0f8002302ed06027efc12ac6bda48
Cory Forsstrom <cforsstrom18@gmail.com>
2024-04-02 14:55:48 -0700

    boulder: Move new command under recipe and alias to boulder new

--- boulder/src/cli.rs
+++ boulder/src/cli.rs
@@ -9,7 +9,6 @@ use thiserror::Error;
 
 mod build;
 mod chroot;
-mod new;
 mod profile;
 mod recipe;
 
@@ -38,25 +37,42 @@ pub struct Global {
 pub enum Subcommand {
     Build(build::Command),
     Chroot(chroot::Command),
-    New(new::Command),
     Profile(profile::Command),
     Recipe(recipe::Command),
 }
 
 pub fn process() -> Result<(), Error> {
-    let Command { global, subcommand } = Command::parse();
+    let args = replace_aliases(std::env::args());
+    let Command { global, subcommand } = Command::parse_from(args);
 
     let env = Env::new(global.cache_dir, global.config_dir, global.data_dir, global.moss_root)?;
 
     match subcommand {
         Subcommand::Build(command) => build::handle(command, env)?,
         Subcommand::Chroot(command) => chroot::handle(command, env)?,
-        Subcommand::New(command) => new::handle(command)?,
         Subcommand::Profile(command) => profile::handle(command, env)?,
         Subcommand::Recipe(command) => recipe::handle(command)?,
     }
 
     Ok(())
+}
+
+fn replace_aliases(args: std::env::Args) -> Vec<String> {
+    const ALIASES: &[(&str, &[&str])] = &[("new", &["recipe", "new"])];
+
+    let mut args = args.collect::<Vec<_>>();
+
+    for (alias, replacements) in ALIASES {
+        let Some(pos) = args.iter().position(|a| a == *alias) else {
+            continue;
+        };
+
+        args.splice(pos..pos + 1, replacements.iter().map(|arg| arg.to_string()));
+
+        break;
+    }
+
+    args
 }
 
 #[derive(Debug, Error)]
@@ -65,8 +81,6 @@ pub enum Error {
     Build(#[from] build::Error),
     #[error("chroot")]
     Chroot(#[from] chroot::Error),
-    #[error("new")]
-    New(#[from] new::Error),
     #[error("profile")]
     Profile(#[from] profile::Error),
     #[error("env")]
